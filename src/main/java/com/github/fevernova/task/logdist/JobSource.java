3d89c470f9d389f9aba77aa7f9d2536b50a045ac
peiliping <peilipingplp@gmail.com>
2019-12-23 22:14:34 +0800

    修复一个logdist的source的bug

--- src/main/java/com/github/fevernova/task/logdist/JobSource.java
+++ src/main/java/com/github/fevernova/task/logdist/JobSource.java
@@ -3,17 +3,25 @@ package com.github.fevernova.task.logdist;
 
 import com.github.fevernova.framework.common.context.GlobalContext;
 import com.github.fevernova.framework.common.context.TaskContext;
+import com.github.fevernova.framework.common.data.BarrierData;
 import com.github.fevernova.framework.common.data.broadcast.GlobalOnceData;
 import com.github.fevernova.framework.component.channel.ChannelProxy;
+import com.github.fevernova.framework.service.barrier.listener.BarrierCoordinatorListener;
 import com.github.fevernova.framework.service.checkpoint.CheckPointSaverPlus;
+import com.github.fevernova.framework.service.state.StateValue;
+import com.github.fevernova.kafka.data.KafkaCheckPoint;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 
 @Slf4j
-public class JobSource extends com.github.fevernova.task.mirrormaker.JobSource {
+public class JobSource extends com.github.fevernova.task.mirrormaker.JobSource implements BarrierCoordinatorListener {
 
 
     private boolean broadCastOnStart;
+
+    private final AtomicBoolean canBeCommitted = new AtomicBoolean(false);
 
 
     public JobSource(GlobalContext globalContext, TaskContext taskContext, int index, int inputsNum, ChannelProxy channelProxy) {
@@ -30,6 +38,38 @@ public class JobSource extends com.github.fevernova.task.mirrormaker.JobSource {
         super.onStart();
         if (this.broadCastOnStart) {
             onBroadcastData(new GlobalOnceData());
+        }
+    }
+
+
+    @Override public boolean collect(BarrierData barrierData) throws Exception {
+
+        return super.checkpoints.getCheckPoint(barrierData.getBarrierId()) != null;
+    }
+
+
+    @Override public StateValue getStateForRecovery(BarrierData barrierData) {
+
+        KafkaCheckPoint checkPoint = super.checkpoints.getCheckPoint(barrierData.getBarrierId());
+        StateValue stateValue = new StateValue();
+        stateValue.setComponentType(super.componentType);
+        stateValue.setComponentTotalNum(super.total);
+        stateValue.setCompomentIndex(super.index);
+        stateValue.setValue(checkPoint);
+        return stateValue;
+    }
+
+
+    @Override public void result(boolean result, BarrierData barrierData) throws Exception {
+
+        this.canBeCommitted.set(result);
+    }
+
+
+    @Override public void completed(BarrierData barrierData) throws Exception {
+
+        if (this.canBeCommitted.get()) {
+            super.completed(barrierData);
         }
     }
 }
